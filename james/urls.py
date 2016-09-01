4f188b4c00868163bd9a18223c00e6a333a69e43
Eimantas <eimantas.stonys@gmail.com>
2016-09-01 23:50:50 +0200

    Hello world view

--- james/urls.py
+++ james/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from james.views import hello
+
 urlpatterns = [
+    url(r'^$', hello, name='hello'),
     url(r'^admin/', admin.site.urls),
 ]
